f8e834da735493b8af9adc1390cc57a7a55ee52b
Srinivash Dev <srinnivassh@gmail.com>
2023-12-18 19:21:35 +0530

    Initial commit

---
+++ android/app/src/main/java/com/ecom/app/MainActivity.java
@@ -0,0 +1,8 @@
+package com.ecom.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+
+}
+
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+            <p className="mb-3 mb-md-0">An<br/><img className="Logo1 "  src="./assets/Loo.png" alt="" /><br/>Creation
+              
+            </p>
+          
+              
+            
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
